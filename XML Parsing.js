60f5c41a0be90b939ea810ca5b24aa7a1f0fc710
Jody Lent <jodylent@gmail.com>
2015-09-03 10:27:52 -0700

    checking a way to pull just the NAME of the scriptable task out:

--- XML Parsing.js
+++ XML Parsing.js
@@ -12,18 +12,17 @@ var document = XMLManager.fromString(xmlString);
 
 //System.log("DOCUMENT IS :  \n\n"+document)
 
-var nodeList = document.getElementsByTagName("workflow-item") ;
+var workflowItemElementList = document.getElementsByTagName("workflow-item") ;
 
-for (var i = 0 ; i < nodeList.length ; i++) {
-	var node = nodeList.item(i) ;
-	if (node.getAttribute("type") == "task") {
-		var name = node.getAttribute("name") ;
-		System.log("Found node with name:  " + name) ;
-//		System.log(node.textContent) ;
-		
-		nodeChildren = null; nodeChildren = node.getChildNodes();
-		scriptNode = null ; scriptNode = nodeChildren.getElementsByTagName("script");
-		scriptText = null ; scriptText = scriptNode.textContent;
-		System.log("FOUND SCRIPT \n\n" + scriptText)
+for (var i = 0 ; i < workflowItemElementList.length ; i++) {
+	var workflowItemElement
+	workflowItemElement = workflowItemElementList.item(i) ;
+	if (workflowItemElement.getAttribute("type") == "task") {
+
+		var nameElement = workflowItemElement.getElementsByTagName("display-name")
+		var nameString = nameElement.textContent
+		System.log("FOUND SCRIPTABLE TASK NAMED :  " + name) ;
+
+//		System.log("FOUND SCRIPT FOR "+name +" : \n\n" + scriptText)
 	}
 }
