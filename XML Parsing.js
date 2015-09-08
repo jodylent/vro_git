011dbc91e605a662cb1eaa868694f04f33c29d0b
Jody Lent <jodylent@gmail.com>
2015-09-08 11:44:03 -0500

    outputs array of composite types, handles multiple Scriptable Tasks now

--- XML Parsing.js
+++ XML Parsing.js
@@ -6,11 +6,13 @@ xmlString -- string -- created from a SQL:ActiveRecord
 
 OUTPUTS:
 
-taskDisplayName -- string -- the name of the Scriptable Task from the workflow being backed up
-taskScriptText -- string -- the contents of the Scriptable Task as a giant text string
+taskList -- Array/Composite Type -- an array of objects with 2 string properties:
+	displayName -- string -- the name of the Scriptable Task from the workflow being backed up
+	scriptText -- string -- the contents of the Scriptable Task as a giant text string
 
 */
 
+var taskList = [];
 var document = XMLManager.fromString(xmlString);
 
 //System.log("DOCUMENT IS :  \n\n"+document)
@@ -39,5 +41,11 @@ for (var i = 0 ; i < workflowItemElementList.length ; i++) {
 	System.log("CURRENT TASK NAME : "+taskDisplayName);
 	System.log("CONTENT IS : \n\n" + taskScriptText + "\n");
 	
+	
+	itemToAdd = null;
+	itemToAdd = {"displayName":taskDisplayName, "scriptText":taskScriptText};
+	taskList.push(itemToAdd);
+
+	System.log("ADDED TO taskList, CURRENT SCRIPT ITEM COUNT IS:  " + taskList.length)
 	}
 }
